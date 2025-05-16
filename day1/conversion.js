cb14dc270400541182933e866836f3a6f77f4043
Techsters-101 <sauravr504@gmail.com>
2025-05-16 04:25:20 +0000

    comparison of datatypes

--- day1/conversion.js
+++ day1/conversion.js
@@ -1,8 +1,12 @@
-let num = 0
+// let num = 0
 
-console.log(typeof(num));
-valofnum = Boolean(num)
-console.log(typeof(valofnum));
-console.log(valofnum);
+// console.log(typeof(num));
+// valofnum = Boolean(num)
+// console.log(typeof(valofnum));
+// console.log(valofnum);
+
+// ************************************************conversion of string************************************************
+
+console.log(1+4 + "2" + 3+6);
 
 
